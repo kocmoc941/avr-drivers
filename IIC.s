dae0366fe68efca509c4aac2fe88ed93dc77b023
kocmoc <kocmoc-sp@yandex.ru>
2018-02-16 08:23:01 +0300

    divide i2c functions (start/stop/send)

--- IIC.s
+++ IIC.s
@@ -1,5 +1,8 @@
 .include "m8def.inc"
 
+.dseg
+    eeprom_read: .db 0b10100001
+    eeprom_write: .db 0b10100000
 .cseg
     ;interrupt vectors
     .org 0x00 rjmp reset
@@ -53,7 +56,7 @@ wait_sending:
 ERROR:
     ret
 
-i2c_send:
+i2c_start:
     ldi r16, (1<<TWINT)|(1<<TWSTA)|(1<<TWEN)
     out TWCR, r16
     rcall wait_sending
@@ -62,8 +65,9 @@ i2c_send:
     andi r16, 0xF8
     cpi r16, 0x08 ;START
     brne ERROR
+    ret
 
-    ldi r16, 0x13; SLA_W
+i2c_send_address:
     out TWDR, r16
     ldi r16, (1<<TWINT)|(1<<TWEN)
     out TWCR, r16
@@ -73,8 +77,9 @@ i2c_send:
     andi r16, 0xF8
     cpi r16, 0x18 ;MT_SLA_ACK
     brne ERROR
+    ret
 
-    ldi r16, 0x11; DATA
+i2c_send_data:
     out TWDR, r16
     ldi r16, (1<<TWINT)|(1<<TWEN)
     out TWCR, r16
@@ -84,9 +89,20 @@ i2c_send:
     andi r16, 0xF8
     cpi r16, 0x28; MT_DATA_ACK
     brne ERROR
+    ret
 
+i2c_stop:
     ldi r16, (1<<TWINT)|(1<<TWEN)|(1<<TWSTO)
     out TWCR, r16
+    ret
+
+i2c_send_nack:
+    ldi r16, (0<<TWEA)
+    out TWCR, r16
+    ret
+
+i2c_send:
+
     ret
 
 reset:
