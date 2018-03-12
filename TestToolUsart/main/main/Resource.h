102eabec854b311e15da92d7228b2a660f5f9c71
kocmoc <kocmoc-sp@yandex.ru>
2018-03-12 22:50:06 +0300

    add com port thread/ //TODO:need fix it

--- TestToolUsart/main/main/Resource.h
+++ TestToolUsart/main/main/Resource.h
@@ -3,10 +3,9 @@
 // Used by main.rc
 //
 #define IDD_DIALOG1                     103
-#define IDC_TREE1                       1005
-#define IDC_SLIDER1                     1007
-#define IDC_EDIT1                       1008
-#define IDC_BUTTON1                     1009
+#define IDC_LOG                         1001
+#define IDC_PORT                        1002
+#define IDC_OPEN_PORT                   1003
 
 // Next default values for new objects
 // 
@@ -14,7 +13,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        105
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1010
+#define _APS_NEXT_CONTROL_VALUE         1011
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
