d24dfec5ed5a0c5ba9a6a4fc8c1239908e13dae5
Denis Timonin <denis.timonin@semsotec.com>
2018-10-30 18:49:35 +0300

    TODO:test USART

--- src/main_slave.s
+++ src/main_slave.s
@@ -3,7 +3,8 @@
 .eseg
     test: .db 1,2,3,0
 .dseg
-    buffer: .byte $50
+    ;buffer: .byte '1'
+    buffer: .db "test",0
 .cseg
     ;interrupt vectors
     .org 0x00 rjmp reset
@@ -18,17 +19,51 @@ error_str: .db "data:", $0
 
 reset:
 .equ F_CPU = 8000000
-.equ BAUD = 38400
+.equ BAUD = 4800
+    ldi r30, low(RAMEND)
+    out SPL, r30
+    ldi r30, high(RAMEND)
+    out SPH, r30
+
     ldi r17, high(F_CPU/(BAUD*16) - 1)
     ldi r16, low (F_CPU/(BAUD*16) - 1)
     rcall usart_init
 
-clr x
+ldi ZL, low(buffer)
+ldi ZH, high(buffer)
+ldi r20, 0
 main:
-    ldi r16, xl
-    inc xl
+    ldi r16, 200
+    rcall delay_ms
+    ;add ZL, r20
+    ld r16, Z
+    cpi r16, 0
+    brne skip
+    ldi ZL, low(buffer)
+    clr r20
+    ld r16, Z
+    skip:
+    inc r20
+    ldi r16, '0'
     rcall usart_send
 rjmp main
+
+delay_ms:
+    .equ operation = 1600
+    ldi yl, high(operation)
+    mul yl, r16
+    mov yl, r0
+    _delay_ms:
+        dec yl
+        ldi yh, low(operation)
+        mul yh, r16
+        mov yh, r0
+        __delay_ms:
+            dec yh
+            brne __delay_ms
+    cpi yl, $0
+    brne _delay_ms
+ret
 
 usart_rx_int:
 reti
