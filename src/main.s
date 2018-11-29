9f00ba5d39f83f1a3309d7cf0e0415a772933566
Denis Timonin <denis.timonin@semsotec.com>
2018-11-29 18:11:06 +0300

    rename file to main.s/add the second mega8a for test usart interrupt

--- src/main.s
+++ src/main.s
@@ -1,112 +1,140 @@
-.list
+.nolist
 
+;traces
+#define PRINT_RAW
+#undef PRINT_RAW
+
+.equ F_CPU = 8000000
+.equ BAUD = 38400
+
+.eseg
+    test: .byte 124
+.dseg
+    var: .byte 3
+    error_status: .byte 1
+    usart_data: .byte 128
+    usart_ptr: .byte 1
 .cseg
     ;interrupt vectors
     .org 0x00 rjmp reset
-    .org 0x01 rjmp int0_int
-    .org 0x02 rjmp int1_int
-    .org 0x03 rjmp timer2_comp
-    .org 0x04 rjmp timer2_ovf
-    .org 0x05 rjmp timer1_capt
-    .org 0x06 rjmp timer1_compa
-    .org 0x07 rjmp timer1_compb
-    .org 0x08 rjmp timer1_ovf
-    .org 0x09 rjmp timer0_ovf
-    .org 0x0A rjmp spi_stc_complete
-    .org 0x0B rjmp usart_rxc_complete
-    .org 0x0C rjmp usart_udre_empty
-    .org 0x0D rjmp usart_tx_complete
-    .org 0x0E rjmp adc_conversion_complete
-    .org 0x0F rjmp eeprom_ready
-    .org 0x10 rjmp analog_comparator
-    .org 0x11 rjmp i2c
-    .org 0x12 rjmp spm_ready
+    .org 0x0B rjmp usart_rx_int
+    .org 0x0C rjmp usart_datae_int
+    .org 0x0D rjmp usart_tx_int
 
 .org 0x13 
 .include "USART.s"
-.include "I2c_master.s"
-error_str: .db "data: ", $0
+.include "utils.s"
+.include "defines.inc"
 
-int0_int:
-int1_int:
-timer2_comp:
-timer2_ovf:
-timer1_capt:
-timer1_compa:
-timer1_compb:
-timer1_ovf:
-timer0_ovf:
-spi_stc_complete:
-usart_rxc_complete:
-usart_udre_empty:
-usart_tx_complete:
-adc_conversion_complete:
-eeprom_ready:
-analog_comparator:
-i2c:
-spm_ready:
+.list
+msg_data: .db "data:", $0
+msg_error: .db "error:", $0, $0
+msg_address: .db "big-endian:", $0
+msg_init: .db "initialize...", $0
+msg_before: .db "first:", 0, 0
+msg_after: .db "second:", 0
+msg_wdt_reset: .db "watchdog reset", 0, 0
 
-print:
-    lpm r16, Z+
-    rcall usart_send
-    cpi r16, 0
-    brne print
-    ret
-    
+
 reset:
-.equ F_CPU = 8000000
-.equ BAUD = 1200
-.equ EEPROM_ADDRESS = 0b0110000
+    ldi r16, low(RAMEND)
+    out SPL, r16
+    ldi r16, high(RAMEND)
+    out SPH, r16
+
     ldi r17, high(F_CPU/(BAUD*16) - 1)
     ldi r16, low (F_CPU/(BAUD*16) - 1)
     rcall usart_init
-    ldi ZH, high(error_str*2)
-    ldi ZL, low(error_str*2)
-    rcall print
 
+    ; save var
+    ldi ZL, low(var)
+    ldi ZH, high(var)
+    ldi r16, $39
+    st Z+, r16
+    inc r16
+    st Z+, r16
+    ldi r16, 255
+    st Z+, r16
+
+    ldi ZL, low(usart_ptr)
+    ldi ZH, high(usart_ptr)
     clr r16
-    rcall i2c_init
-    main:
-    ; write EEPROM
-    rcall i2c_start
-    ldi r16, EEPROM_ADDRESS
-    lsl r16
-    ori r16, 0
-    rcall i2c_send_byte
-    rcall i2c_get_status
-    rcall usart_send
-    rcall delay
-    ldi r16, $11
-    rcall i2c_send_byte
-    rcall i2c_get_status
-    rcall usart_send
-    rcall delay
-    ldi r16, $F0
-    rcall i2c_send_byte
-    rcall i2c_get_status
-    rcall usart_send
-    rcall i2c_stop
-    rcall delay
-    ldi r16, $4
-    rcall usart_send
-    
-    rjmp main
+    st Z, r16
 
+in r16, MCUCSR
+cpi r16, 1<<WDRF
+brne _init
+PRINTS msg_wdt_reset
+rjmp _def
+    _init:
+    PRINTS msg_init
+    _def:
+    PRINTEOL
 
-    delay:
-        ldi r16, $FF
-        loop:
-        ldi r17, $FF
-        inner17:
-                ldi r18, $6
-                inner18:
-                dec r18
-                tst r18
-                brne inner18
-            dec r17
-            tst r17
-            brne inner17
-        dec r16
-        tst r16
-        brne loop
-        ret
+    ;START_WDT 4
+
+    ldi r16, usart_data
+    ldi ZL, low(usart_ptr)
+    ldi ZH, low(usart_ptr)
+    st Z, r16
+
+_main:
+;wdr
+    DELAY $18630B
+    PRINTS msg_data
+
+    PRINTS msg_address
+
+    PRINTV var, 3
+
+    PRINTEOL
+
+    EEPROM_R 0
+
+    cpi r20, 8
+        brne _n1
+        PRINTB '1'
+        rjmp def
+    _n1:
+        mov r16, r20
+        rcall to_str_hex
+        PRINTR r20
+        PRINTR r21
+        EEPROM_W 0, 8
+    def:
+    PRINTEOL
+rjmp _main
+
+usart_rx_int:
+    in r17, UDR
+    clr ZH
+    ldi ZL, usart_ptr
+    ld r16, Z
+
+    cpi r16, 128
+    brcc _norm
+        clr r16
+        st Z, r16
+    _norm:
+
+    ldi r18, usart_data
+    add r18, r16
+    mov ZL, r18
+    st Z+, r17
+    ldi r16, 0
+    st Z, r16
+
+    ldi ZL, usart_ptr
+    inc r16
+    st Z, r16
+
+    mov r16, r17
+
+reti
+
+usart_datae_int:
+reti
+
+usart_tx_int:
+reti
+
